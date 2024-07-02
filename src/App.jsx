d9632787c90637b111d98308745ddea9ed6c75f8
Ramesh Tudu <rameshtudu3@gmail.com>
2024-07-02 16:30:00 +0545

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "./comnponents/navbar/Navbar"
+
+
+function App() {
+
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/comnponents/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className="bg-white shadow-sm">
+            <div className="container">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
